8eafa804ef34d44091483486d4f4d314767b797e
Saturn Lai <daolvcntt@gmail.com>
2016-07-06 10:22:25 +0000

    dev

--- user/themes/learn2/js/site.js
+++ user/themes/learn2/js/site.js
@@ -1,7 +1,7 @@
 $(function() {
-	// Check event to scroll window is top > 1500 to display
 	$(window).scroll(function() {
-		if ($(this).scrollTop() >50) {
+        var heightNote = $('#pageNote').height();
+		if ($(this).scrollTop() >= heightNote - 10 ) {
 			$('body').addClass('scroll');
 		} else {
 			$('body').removeClass('scroll');
