41ace9fde68caaaee318d8cabd3db9fb4a4a9694
Saturn Lai <daolvcntt@gmail.com>
2016-07-06 02:45:04 +0000

    saturn_help

---
+++ user/themes/learn2/js/site.js
@@ -0,0 +1,11 @@
+$(function() {
+    // Check event to scroll window is top > 1500 to display
+    // $(window).scroll(function() {
+    //     console.log($(this).scrollTop());
+    //     if ($(this).scrollTop() >= 60) {
+    //         $('#sidebar').css({
+    //             'top': 0
+    //         });
+    //     }
+    // });
+});
