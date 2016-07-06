c4272833635b08730130989690c0908c14d2acc8
Saturn Lai <daolvcntt@gmail.com>
2016-07-06 08:22:37 +0000

    dev

--- user/themes/learn2/js/site.js
+++ user/themes/learn2/js/site.js
@@ -1,11 +1,10 @@
 $(function() {
-    // Check event to scroll window is top > 1500 to display
-    // $(window).scroll(function() {
-    //     console.log($(this).scrollTop());
-    //     if ($(this).scrollTop() >= 60) {
-    //         $('#sidebar').css({
-    //             'top': 0
-    //         });
-    //     }
-    // });
+	// Check event to scroll window is top > 1500 to display
+	$(window).scroll(function() {
+		if ($(this).scrollTop() >50) {
+			$('body').addClass('scroll');
+		} else {
+			$('body').removeClass('scroll');
+		}
+	});
 });
