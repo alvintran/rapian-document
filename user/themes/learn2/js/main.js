678df20eb36094ee44aa93aa3142161ac9169ce0
Saturn Lai <daolvcntt@gmail.com>
2016-07-09 03:13:47 +0000

    dev

--- user/themes/learn2/js/main.js
+++ user/themes/learn2/js/main.js
@@ -1,22 +1,22 @@
 $(function() {
     // Check event to scroll window is top > 1500 to display
-    $(window).scroll(function(){
-        var opacity = 1- $(this).scrollTop() / 300 - 0.1;
-        $('.hero_content, .home_scroll, .copyright').css({
-            'opacity': opacity
-        });
+    // $(window).scroll(function(){
+    //     var opacity = 1- $(this).scrollTop() / 300 - 0.1;
+    //     $('.hero_content, .home_scroll, .copyright').css({
+    //         'opacity': opacity
+    //     });
 
-        if ($(document).height() == $(this).scrollTop() + $(window).height()) {
-            $('.copyright').css({
-                'opacity': 1
-            });
-        }
+    //     if ($(document).height() == $(this).scrollTop() + $(window).height()) {
+    //         $('.copyright').css({
+    //             'opacity': 1
+    //         });
+    //     }
 
-        if ($(this).scrollTop() > 70) {
-            $('.header').addClass('scroll');
-        }else {
-            $('.header').removeClass('scroll');
-        }
-    });
+    //     if ($(this).scrollTop() > 70) {
+    //         $('.header').addClass('scroll');
+    //     }else {
+    //         $('.header').removeClass('scroll');
+    //     }
+    // });
     $('.col').matchHeight();
 });
